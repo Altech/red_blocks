ab106ec9f0a9e8e439009becf2ff9edbed05aaa0
rerost <gmailii@yahoo.co.jp>
2019-12-12 15:39:51 +0900

    Fix spec

--- spec/red_blocks/instant_set_spec.rb
+++ spec/red_blocks/instant_set_spec.rb
@@ -21,9 +21,8 @@ describe RedBlocks::InstantSet do
 
   describe '#key_suffix' do
     context 'when value is ids' do
-      let(:value) { [1,2,3] }
-      let(:instant_set1) { RedBlocks::InstantSet.new(value) }
-      let(:instant_set2) { RedBlocks::InstantSet.new(value.reverse) }
+      let(:instant_set1) { RedBlocks::InstantSet.new([1,2,3]) }
+      let(:instant_set2) { RedBlocks::InstantSet.new([1,2,3,4]) }
 
       it 'return value' do
         expect(instant_set1.key_suffix).not_to eq instant_set2.key_suffix
@@ -32,8 +31,8 @@ describe RedBlocks::InstantSet do
 
     context 'when value is id_with_scores' do
       let(:value) { [[1, 1], [2, 2], [3, 3]] }
-      let(:instant_set1) { RedBlocks::InstantSet.new(value) }
-      let(:instant_set2) { RedBlocks::InstantSet.new(value.reverse) }
+      let(:instant_set1) { RedBlocks::InstantSet.new([[1, 1], [2, 2], [3, 3]]) }
+      let(:instant_set2) { RedBlocks::InstantSet.new([[1, 0], [2, 2], [3, 3]]) }
 
       it 'return value' do
         expect(instant_set1.key_suffix).not_to eq instant_set2.key_suffix
